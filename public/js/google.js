97f0852924d2e3f73c16b94bba78417f99984247
CEPL <cepl@CEPLs-MacBook-Air-4.local>
2017-11-10 17:44:28 +0530

    Front-end done

--- public/js/google.js
+++ public/js/google.js
@@ -3,21 +3,23 @@ var map;
 function initMap() {
   navigator.geolocation.getCurrentPosition(success, error);   
   function success(position){
-    var mapOptions = {
+    let mapOptions = {
         zoom: 18,
         center: new google.maps.LatLng(position.coords.latitude,position.coords.longitude),
         mapTypeId: google.maps.MapTypeId.ROADMAP
       };
       map = new google.maps.Map(document.getElementById('map_canvas'),mapOptions);
-      google.maps.event.addListener(map,'center_changed', function() {
-        document.getElementById('default_latitude').value = map.getCenter().lat();
-        document.getElementById('default_longitude').value = map.getCenter().lng();
-      });
+  }
+  function error() {
+      alert("Cant find poition");
   }
 
-  function error() {
-      console.log("Cant find poition");
-  }
 }
 
 window.onload = initMap();
+
+function submitLoc() {
+    let latS = map.getCenter().lat(); 
+    let lngS = map.getCenter().lng();
+    console.log(latS + " " + lngS); 
+}
