457ef09ccc2327c10e4e8aef1b20b0b3c1ba2c74
CEPL <cepl@CEPLs-MacBook-Air-4.local>
2017-11-10 15:22:01 +0530

    Location done

--- public/js/google.js
+++ public/js/google.js
@@ -1,27 +1,23 @@
 var map;
+
 function initMap() {
-  var mapOptions = {
-      zoom: 18,
-      center: new google.maps.LatLng(12.9716,77.5946),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    map = new google.maps.Map(document.getElementById('map_canvas'),
-      mapOptions);
-    google.maps.event.addListener(map,'center_changed', function() {
-      document.getElementById('default_latitude').value = map.getCenter().lat();
-      document.getElementById('default_longitude').value = map.getCenter().lng();
-    });
-    $('<div/>').addClass('centerMarker').appendTo(map.getDiv())
-      //do something onclick
-      .click(function() {
-        var that = $(this);
-        if (!that.data('win')) {
-          that.data('win', new google.maps.InfoWindow({
-            content: 'this is the center'
-          }));
-          that.data('win').bindTo('position', map, 'center');
-        }
-        that.data('win').open(map);
+  navigator.geolocation.getCurrentPosition(success, error);   
+  function success(position){
+    var mapOptions = {
+        zoom: 18,
+        center: new google.maps.LatLng(position.coords.latitude,position.coords.longitude),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      map = new google.maps.Map(document.getElementById('map_canvas'),mapOptions);
+      google.maps.event.addListener(map,'center_changed', function() {
+        document.getElementById('default_latitude').value = map.getCenter().lat();
+        document.getElementById('default_longitude').value = map.getCenter().lng();
       });
+  }
+
+  function error() {
+      console.log("Cant find poition");
+  }
 }
-google.maps.event.addDomListener(window, 'load', initMap);
+
+window.onload = initMap();
